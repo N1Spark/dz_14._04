7a00951a9e0016138a73432e3539166e88417599
Sarp_dg92@student.itstep.org <Sarp_dg92@student.itstep.org>
2023-04-17 15:10:42 +0300

    Add project files.

---
+++ 14.04.23_HW/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace dz_14._04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.администраторскийРежимToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::_14._04._23_HW.Properties.Resources._new;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(13, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 42);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::_14._04._23_HW.Properties.Resources.Exit;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(99, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 42);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(199, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "СТОП";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.ForeColor = System.Drawing.Color.Gold;
+            this.groupBox1.Location = new System.Drawing.Point(808, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 82);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подсказки";
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::_14._04._23_HW.Properties.Resources._3;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(197, 21);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 44);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::_14._04._23_HW.Properties.Resources._2;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(106, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 44);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::_14._04._23_HW.Properties.Resources._1;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(16, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 44);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem,
+            this.администраторскийРежимToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1095, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // администраторскийРежимToolStripMenuItem
+            // 
+            this.администраторскийРежимToolStripMenuItem.Name = "администраторскийРежимToolStripMenuItem";
+            this.администраторскийРежимToolStripMenuItem.Size = new System.Drawing.Size(217, 24);
+            this.администраторскийРежимToolStripMenuItem.Text = "Администраторский режим";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Enabled = false;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.Color.Gold;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listBox1.ItemHeight = 36;
+            this.listBox1.Location = new System.Drawing.Point(841, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listBox1.Size = new System.Drawing.Size(254, 540);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.Location = new System.Drawing.Point(12, 389);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(823, 111);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "ВОпрос";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.ForeColor = System.Drawing.Color.Gold;
+            this.button7.Location = new System.Drawing.Point(13, 507);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(383, 76);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "1";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.ForeColor = System.Drawing.Color.Gold;
+            this.button8.Location = new System.Drawing.Point(452, 506);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(383, 76);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "2";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Black;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.ForeColor = System.Drawing.Color.Gold;
+            this.button9.Location = new System.Drawing.Point(13, 595);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(383, 76);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "3";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Black;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.ForeColor = System.Drawing.Color.Gold;
+            this.button10.Location = new System.Drawing.Point(452, 595);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(383, 76);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "4";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(420, 132);
+            this.panel1.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::_14._04._23_HW.Properties.Resources.zvonok;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 134);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(149, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Максим";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(148, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Максим";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = global::_14._04._23_HW.Properties.Resources.mil;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1095, 683);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem администраторскийРежимToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 14.04.23_HW/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms.VisualStyles;
+
+namespace _14._04._23_HW
+{
+    public partial class Form1 : Form
+    {
+        string cor_answ;
+        string[] lines;
+        List<string> answers, shuffle;
+        Random rand = new Random();
+        int count = 0, answ = 14;
+        List<Button> buttons;
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible = false;
+            buttons = new List<Button> { button7, button8, button9, button10};
+            BackgroundImage = Properties.Resources.mil;
+            string[] strings = { "15 - 1 000 000", "14 - 500 000", "13 - 250 000" , "12 - 125 000", "11 - 64 000" , "10 - 32 000",
+            "9 - 16 000", "8 - 8 000", "7 - 4 000", "6 - 2 000", "5 - 1 000", "4 - 500", "3 - 300", "2 - 200", "1 - 100"};
+            listBox1.Items.AddRange(strings);
+            lines = File.ReadAllLines("C:\\Users\\mohse\\source\\repos\\14.04.23_HW\\14.04.23_HW\\res\\question.txt");
+            Start();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            bool check;
+            foreach (Button x in buttons)
+                x.Enabled = false;
+            Button button = (Button)sender;
+            if (button.Text == cor_answ)
+            {
+                button.BackColor = Color.Green;
+                check = true;
+            }
+            else
+            {
+                button.BackColor = Color.Red;
+                foreach(Button x in buttons)
+                    if(x.Text == cor_answ)
+                        button = (Button)x;
+                button.BackColor = Color.Green;
+                check = false;
+            }
+            Game(check);
+        }
+        private void Game(bool check)
+        {
+            foreach (Button x in buttons)
+            {
+                x.Enabled = true;
+                x.BackColor = Color.Black;
+            }
+            Thread.Sleep(2000);
+            if (check)
+            {
+                panel1.Visible = false;
+                textBox1.Text = lines[count];
+                cor_answ = lines[count + 1];
+                answers = new List<string> { lines[count + 1], lines[count + 2], lines[count + 3], lines[count + 4] };
+                shuffle = answers.OrderBy(x => rand.Next()).ToList();
+                button7.Text = shuffle[0];
+                button8.Text = shuffle[1];
+                button9.Text = shuffle[2];
+                button10.Text = shuffle[3];
+                listBox1.SelectedIndex = answ;
+                answ--;
+                count += 5;
+            }
+            else
+            {
+                foreach (Button x in buttons)
+                    x.Enabled = false;
+                textBox1.Text = "You lose";
+                listBox1.SelectedIndex = -1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel1.Visible= true;
+            label2.Text = "Я думаю это...\n";
+            int rand_a = rand.Next(1, 100);
+            if (rand_a <= 30)
+                label2.Text += cor_answ;
+            else
+            {
+                rand_a = rand.Next(0, 3);
+                label2.Text += buttons[rand_a].Text;
+            }
+            button5.Enabled = false;
+            button5.BackgroundImage = Properties.Resources._5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Button x in buttons)
+                x.Enabled = false;
+            string text = "Вы выиграли";
+            string temp = listBox1.SelectedItem.ToString();
+            int ind = temp.IndexOf(" - ");
+            textBox1.Text = text;
+            for (int i = ind; i < temp.Length; i++)
+                textBox1.Text += temp[i];
+            textBox1.Text += "$";
+        }
+        private void Start()
+        {
+            textBox1.Text = lines[0];
+            var item = listBox1.Items[14];
+            cor_answ = lines[1];
+            answers = new List<string> { lines[1], lines[2], lines[3], lines[4] };
+            shuffle = answers.OrderBy(x => rand.Next()).ToList();
+            button7.Text = shuffle[0];
+            button8.Text = shuffle[1];
+            button9.Text = shuffle[2];
+            button10.Text = shuffle[3];
+            listBox1.SelectedIndex = answ;
+            answ--;
+            count += 5;
+        }
+    }
+}
